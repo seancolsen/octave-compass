5d56a0e07c729a27b89413c84b611bd2ef1e95d8
Sean Madsen <sean@seanmadsen.com>
2018-04-29 16:36:32 -0400

    Disable default drag-related behaviors

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Disable default drag-related behaviors
+document.body.ondrag = () => false;
+document.body.ondragstart = () => false;
+document.body.ondrop = () => false;
+document.body.ondblclick = () => false;
+
 registerServiceWorker();
