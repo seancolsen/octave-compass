35f6ede595838f98c878daa73223197090e673d7
Sean Madsen <sean@seanmadsen.com>
2018-04-29 22:36:02 -0400

    Use state to manage rotation

---
+++ src/Architecture.md
@@ -0,0 +1,37 @@
+## Components
+
+* App
+  * TonalCenterIndicator
+  * ScaleIndicator
+  * Wheel
+    * Base
+      * Interval
+        * Checkbox
+        * IntervalLabel
+    * Keyboard
+      * Key
+        * Polygon
+        * Note
+    * Scale
+      * Polygon
+      * ScaleChord
+        * ChordEmblem
+  * Menu
+    * ??
+      * ChordChoice
+        * ChordEmblem
+    
+## State
+
+* App
+  * intervals
+  * tonalCenter
+  * chords
+  * keyboardIsRotating
+  * scaleIsRotating
+  
+* Keyboard
+  * Rotation
+  
+* Scale
+  * Rotation
--- src/Scale.js
+++ src/Scale.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Polygon from "./Polygon.js";
 
 const RADIUS = 56;
 
-export default function Scale(props) {
-  let intervals = [0, 2, 4, 5, 7, 9, 11];
-  let points = intervals.map(v => [v, RADIUS]);
-  return (
-    <Polygon points={points} rotation={0}/>
-  );
+export default class Scale extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      rotation: 0
+    };
+  }
+
+  render() {
+    let intervals = [0, 2, 4, 5, 7, 9, 11];
+    let points = intervals.map(v => [v, RADIUS]);
+    return (
+      <Polygon points={points} rotation={this.state.rotation}/>
+    );
+  }
+
 }
