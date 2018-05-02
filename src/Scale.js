ae2959755463a5bb522dcf80d6ad0edf378b98eb
Sean Madsen <sean@seanmadsen.com>
2018-05-02 10:28:59 -0400

    Move rotation state up to wheel

--- src/Scale.js
+++ src/Scale.js
@@ -1,35 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Polygon from "./Polygon.js";
 import Group from './Group.js';
 
 const RADIUS = 56;
 
-export default class Scale extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      rotation: 0
-    };
-  }
-
-  handleMouseMove = (e) => {
-    this.setState({
-      rotation: e.clientX / 100,
-    });
-  };
-
-  render() {
-    let intervals = [0, 2, 4, 5, 7, 9, 11];
-    let points = intervals.map(v => [v, RADIUS]);
-    return (
-      <Group
-        onMouseMove={this.handleMouseMove}
-        rotation={this.state.rotation}
-      >
-        <Polygon points={points}/>
-      </Group>
-    );
-  }
-
+export default function Scale(props) {
+  let intervals = [0, 2, 4, 5, 7, 9, 11];
+  let points = intervals.map(v => [v, RADIUS]);
+  return (
+    <Group rotation={props.rotation}>
+      <Polygon points={points}/>
+    </Group>
+  );
 }
--- src/Wheel.js
+++ src/Wheel.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Keyboard from "./Keyboard.js";
 import Scale from "./Scale.js";
 
-export default function Wheel(props) {
-  return (
-    <svg viewBox={'-100 -100 200 200'}>
-      <Keyboard/>
-      <Scale/>
-    </svg>
-  );
+export default class Wheel extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyboardRotation: 0,
+      scaleRotation: 0
+    };
+  }
+
+  handleMouseMove = (e) => {
+    this.setState({
+      scaleRotation: e.clientX / 100,
+    });
+  };
+
+  render() {
+    return (
+      <svg
+        viewBox={'-100 -100 200 200'}
+        onMouseMove={this.handleMouseMove}
+      >
+        <Keyboard/>
+        <Scale rotation={this.state.scaleRotation}/>
+      </svg>
+    );
+  }
 }
