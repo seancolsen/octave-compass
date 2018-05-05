4609c817f4749f05b9a14efeceee42c2df21b4fc
Sean Madsen <sean@seanmadsen.com>
2018-05-05 16:15:46 -0400

    Improve position of key labels

--- src/KeyLabel.js
+++ src/KeyLabel.js
@@ -2,18 +2,24 @@ import React, {Component} from 'react';
 import IrPoint from "./Utils/IrPoint";
 import styled from 'styled-components';
 
-const RADIUS = 60;
+const RADIUS = 64;
 
 function KeyLabel(props) {
   let point = (new IrPoint(props.interval, RADIUS)).toXy();
+  let transform = `rotate(0) translate(${point.x} ${point.y})`;
   return (
-    <text className={props.className} x={point.x} y={point.y}>
+    <text
+      className={props.className}
+      dominantBaseline={'middle'} // TODO address lack of IE support
+      textAnchor={'middle'}
+      transform={transform}
+    >
       {props.children}
     </text>
   );
 }
 
 export default styled(KeyLabel)`
-  font-size: 10px;
+  font-size: 8px;
   fill: black;
 `;
