b1accce94c45a60ccbbb82374c99d4d0647ae00f
Sean Madsen <sean@seanmadsen.com>
2018-05-05 21:20:52 -0400

    Adapt note names based on tonal center

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -3,27 +3,46 @@ import Key from "./Key.js";
 import {musicTheory} from "./Data/musicTheory.js";
 import Note from './Utils/Note';
 import Rotatable from "./Rotatable";
+import Scalar from './Utils/Scalar';
 
 class Keyboard extends Component {
-  render() {
-    const keys = musicTheory.notes.map((noteData, index) => {
+
+  /**
+   * Determine which types of names to use for keys based on the rotation of the
+   * keyboard.
+   */
+  useNames() {
+    const divisions = musicTheory.octaveDivisions;
+    const roundRotation = Math.round(this.props.rotation);
+    const interval = Scalar.wrap(divisions - roundRotation, divisions);
+    const note = new Note(musicTheory.notes[interval]);
+    return note.useNames;
+  }
+
+  /**
+   * Generate array of Key components
+   *
+   * @return {any[]}
+   */
+  keys() {
+    return musicTheory.notes.map((noteData, index) => {
       const note = new Note(noteData);
       return (
         <Key
           key={index}
-          label={note.name('flat')}
+          label={note.name(this.useNames())}
           color={note.color}
           interval={index}
           rotation={this.props.rotation}
         />
       );
     });
-    return (
-      <g>
-        {keys}
-      </g>
-    );
   }
+
+  render() {
+    return <g>{this.keys()}</g>;
+  }
+
 }
 
 export default Rotatable(Keyboard);
