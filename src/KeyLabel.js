ae633c2ae229d926c0633292f73793b6171d5e37
Sean Madsen <sean@seanmadsen.com>
2018-05-05 20:16:38 -0400

    Add a background to the key labels

--- src/KeyLabel.js
+++ src/KeyLabel.js
@@ -3,25 +3,29 @@ import IrPoint from "./Utils/IrPoint";
 import styled from 'styled-components';
 import Angle from "./Utils/Angle";
 
-const RADIUS = 64;
+const RADIUS = 64.5;
 
 function KeyLabel(props) {
   let point = (new IrPoint(props.interval, RADIUS)).toXy();
   let rotation = -Angle.iToD(props.rotation);
   let transform = `translate(${point.x} ${point.y}) rotate(${rotation})`;
   return (
-    <text
-      className={props.className}
-      dominantBaseline={'middle'} // TODO address lack of IE support
-      textAnchor={'middle'}
-      transform={transform}
-    >
-      {props.children}
-    </text>
+    <g transform={transform}>
+      <rect x="-6" y="-6" width="12" height="12" rx={3}  ry={3}/>
+      <text
+        x={0}
+        y={0.75}
+        className={props.className}
+        dominantBaseline={'middle'} // TODO address lack of IE support
+        textAnchor={'middle'}
+      >
+        {props.children}
+      </text>
+    </g>
   );
 }
 
 export default styled(KeyLabel)`
-  font-size: 8px;
-  fill: black;
+  font-size: 7px;
+  fill: white;
 `;
