c1f1055491cacbdd41fbd649ce873f8ed3ccf05e
Sean Madsen <sean@seanmadsen.com>
2018-05-11 14:50:56 -0400

    Display one unified shadow behind all active keys

--- src/KeySet.js
+++ src/KeySet.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Key from "./Key";
+import styled from 'styled-components';
+
+const StyledG = styled.g`
+  filter: ${props => props.active ? 'drop-shadow( 0 0 10px #000 )' : 'none'};
+`;
 
 export default function KeySet(props) {
-  return props.noteSet.notes.map(note => {
-    return (
-      <g>
-        <Key
-          key={note.id}
-          note={note}
-          rotation={props.rotation}
-          active={props.active}
-        />
-      </g>
-    );
-  });
+  return (
+    <StyledG active={props.active}>
+      {
+        props.noteSet.notes.map(note =>
+          <Key
+            key={note.id}
+            note={note}
+            rotation={props.rotation}
+            active={props.active}
+          />
+        )
+      }
+    </StyledG>
+  );
 }
