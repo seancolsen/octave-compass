bad6f8efb036a1469600e3e5a420c36ba583a12f
Sean Madsen <sean@seanmadsen.com>
2018-05-14 12:01:16 -0400

    Choose a default name to display in the marquee

--- src/Marquee.js
+++ src/Marquee.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledDiv = styled.div`
+const DefaultName = styled.div`
   text-align: center;
   padding: 0.5rem;
 `;
@@ -18,21 +18,46 @@ const NoteName = styled.span`
   font-weight: bold;
 `;
 
-export default function Marquee(props) {
+const AlternateNames = styled(DefaultName)`
+  font-size: 75%;
+  font-style: italic;
+  height: 1em;
+`;
+
+const Aka = styled.span`
+  color: #333;
+`;
+
+function Marquee(props) {
   const tonalCenter = props.noteSet.firstNote.namesToUseForLabels
     .map(name => name.unicode).join('/');
-  const name = props.intervalSet.names.join(" / ");
+  const alternateNames = props.intervalSet.alternateNames;
+  const alternateNamesString = alternateNames ? alternateNames.join(", ") : '';
+  const defaultName = props.intervalSet.defaultName;
   return (
-    <StyledDiv className={props.className}>
-      {name ?
-        <IntervalSetName>{name}</IntervalSetName> :
-        <UnknownName>unknown scale</UnknownName>
-      }
-      <span> in </span>
-      <NoteName>
-        {tonalCenter}
-      </NoteName>
-      <span> ({props.intervalSet.binary})</span>
-    </StyledDiv>
+    <div className={props.className}>
+      <DefaultName>
+        {defaultName ?
+          <IntervalSetName>{defaultName}</IntervalSetName> :
+          <UnknownName>unknown scale</UnknownName>
+        }
+        <span> in </span>
+        <NoteName>
+          {tonalCenter}
+        </NoteName>
+        <span> ({props.intervalSet.binary})</span>
+      </DefaultName>
+      <AlternateNames>
+        {alternateNamesString ?
+          [<Aka>Also know as: </Aka>,
+          <span>{alternateNamesString}</span>]
+          : ""
+        }
+      </AlternateNames>
+    </div>
   );
 }
+
+export default styled(Marquee)`
+  margin-bottom: 1em;
+`;
--- src/Utils/Scale.js
+++ src/Utils/Scale.js
@@ -4,13 +4,14 @@ import {allScales} from "../Data/allScales";
 export default class Scale extends IntervalSet {
 
   constructor(binary) {
-    const scaleData = allScales[binary];
-    if (!scaleData) {
+    const scaleData = allScales[binary] || [];
+    if (scaleData.length < 1) {
       throw "Unknown scale";
     }
     super(binary);
-    this.names = scaleData || [];
+    this.names = scaleData;
+    this.defaultName = scaleData[0];
+    this.alternateNames = scaleData.slice(1);
   }
-
 
 }
