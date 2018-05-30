d34a862aa076139dc467f05e8c4c036f614da2ac
Sean Madsen <sean@seanmadsen.com>
2018-05-30 11:55:58 -0400

    Minor refactoring of Notation component

--- src/Notation.js
+++ src/Notation.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
 import Vex from 'vexflow';
 
+const VF = Vex.Flow;
+
 export default class Notation extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clef: "treble",
+    };
+  }
 
   componentDidMount() {
     this.draw();
@@ -11,33 +20,19 @@ export default class Notation extends Component {
     this.draw();
   }
 
-  draw() {
-    const VF = Vex.Flow;
-    let div = document.getElementById("notation");
-    if (div && div.firstChild) {
-      div.firstChild.remove();
-    }
-    let renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-    renderer.resize(500, 100);
-    let context = renderer.getContext();
-    let stave = new VF.Stave(0, 0, 400);
-
-    // TODO: Allow user to change clef
-    const clef = "treble";
-    stave.addClef(clef);
+  /**
+   * Return an array containing one VF StaveNote for each pitch in our NoteSet
+   *
+   * @return {StaveNote[]}
+   */
+  staveNotes() {
     const octave = 4;
-
-    // TODO: determine appropriate key signature and set it here
-    //stave.addKeySignature('Bb');
-
-    stave.setContext(context).draw();
-
     const pitches = this.props.noteSet.pitchSetStartingFrom(octave).pitches;
-    const staveNotes = pitches.map(pitch => {
+    return pitches.map(pitch => {
       let staveNote = new VF.StaveNote({
-        clef: clef,
+        clef: this.state.clef,
         keys: [pitch.slashNotation],
-        duration: 'q'
+        duration: 'q',
       });
       let modifier = pitch.note.guaranteedName.modifier.ascii;
       if (modifier) {
@@ -45,7 +40,31 @@ export default class Notation extends Component {
       }
       return staveNote;
     });
+  }
 
+  /**
+   * Refresh the notation SVG
+   */
+  draw() {
+    let containingDiv = document.getElementById("notation");
+
+    // If the SVG already exists, remove it
+    if (containingDiv && containingDiv.firstChild) {
+      containingDiv.firstChild.remove();
+    }
+
+    // Set up renderer
+    let renderer = new VF.Renderer(containingDiv, VF.Renderer.Backends.SVG);
+    renderer.resize(400, 100);
+    let context = renderer.getContext();
+
+    // Set up stave
+    let stave = new VF.Stave(0, 0, 400);
+    stave.addClef(this.state.clef);
+    stave.setContext(context).draw();
+
+    // Add notes to stave
+    const staveNotes = this.staveNotes();
     let voice = new VF.Voice({num_beats: staveNotes.length, beat_value: 4});
     voice.addTickables(staveNotes);
     new VF.Formatter().joinVoices([voice]).format([voice], 400);
@@ -57,4 +76,5 @@ export default class Notation extends Component {
       <div id='notation' className={this.props.className}/>
     );
   }
+
 }
