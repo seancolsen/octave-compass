10c8fc928ee75825a54a5b1594a2cb704746b2c7
Sean Madsen <sean@seanmadsen.com>
2018-06-11 12:22:29 -0400

    Refactor ObjectLog tests to not depend on Cord

--- src/Utils/Misc/ObjectLog.test.js
+++ src/Utils/Misc/ObjectLog.test.js
@@ -1,35 +1,31 @@
 import ObjectLog from "Utils/Misc/ObjectLog";
-import OrdinalChord from "Utils/Music/OrdinalChord";
-import Chord from "Utils/Music/Chord";
 
 test('constructor empty', () => {
-  let ocl = new ObjectLog();
-  expect(ocl.log.size).toBe(0);
+  let ol = new ObjectLog();
+  expect(ol.log.size).toBe(0);
 });
 
-test('add', () => {
-  let ocl = new ObjectLog();
-  ocl = ocl.add(new OrdinalChord(0, Chord.fromName('major')));
-  expect(ocl.log.size).toBe(1);
+test('add single entries', () => {
+  let ol = new ObjectLog();
+  ol = ol.add("foo");
+  expect(ol.log.size).toBe(1);
 });
 
 test('maxKey', () => {
-  let ocl = new ObjectLog();
-  expect(ocl.maxKey).toBe(0);
-  ocl = ocl.add(new OrdinalChord(0, Chord.fromName('major')));
-  expect(ocl.maxKey).toBe(1);
+  let ol = new ObjectLog();
+  expect(ol.maxKey).toBe(0);
+  ol = ol.add("foo");
+  expect(ol.maxKey).toBe(1);
 });
 
 test('old chords should be removed when new chords are added', () => {
   let ol = new ObjectLog(new Map(), 0, 3);
-  ol = ol.add(new OrdinalChord(0, Chord.fromName('major')));
-  ol = ol.add(new OrdinalChord(1, Chord.fromName('minor')));
-  ol = ol.add(new OrdinalChord(10, Chord.fromName('augmented')));
+  ol = ol.add('one');
+  ol = ol.add('two');
+  ol = ol.add('three');
   expect(ol.log.size).toBe(3);
-  ol = ol.add(new OrdinalChord(5, Chord.fromName('diminished')));
+  ol = ol.add('four');
   expect(ol.log.size).toBe(3);
   expect(ol.maxKey).toBe(4);
-  expect([...ol.log].map(([key, ordinalChord]) =>
-    ordinalChord.chord.defaultName
-  )).toEqual(['minor', 'augmented', 'diminished']);
+  expect([...ol.log.values()]).toEqual(['two', 'three', 'four']);
 });
