7cc250eff02181f4b337cc5d294f96b44aab854c
Sean Colsen <colsen.sean@gmail.com>
2020-08-23 14:38:45 -0400

    Discover conflict with setPointerCapture and state
    In useRotator.tsx:startRotating(), I call el.setPointerCapture()
    This call works if I don't modify state, but fails silenly if I do. WTF?
    I'm commiting this in case I need to come back to it, but it definitely
    doesn't work.

--- src/components/Wheel/useRotator.tsx
+++ src/components/Wheel/useRotator.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { Scalar } from '../../Utils/Math/Scalar';
 import { Ordinal } from '../../Utils/Music/Ordinal';
 import { Group } from './common/Group';
-import { useLocalStore, useObserver } from 'mobx-react-lite';
 import { XyPoint } from '../../Utils/Geometry/XyPoint';
 
 
@@ -12,7 +11,7 @@ import { XyPoint } from '../../Utils/Geometry/XyPoint';
  * determine the center of rotation, by assuming that it's at the center of the
  * SVG -- an assumption that, for the time being is hard-coded into this app.
  */
-const centerOfContainingSvg = (e: PointerEvent) => {
+const centerOfContainingSvg = (e: React.PointerEvent) => {
   const target = e.target as SVGElement;
   const svg = (target.tagName === "svg") ? target : target.viewportElement;
   if (!svg) {
@@ -59,168 +58,191 @@ export function useRotator(props: Props) {
 
   const ref = React.createRef<SVGGElement>();
 
-  const state = useLocalStore(() => ({
-
-    /**
-     * The point (in page space) around which the object rotates. Null when we
-     * don't know yet.
-     */
-    center: null as XyPoint | null,
-
-    /**
-     * The current value of rotation, which changes constantly as the user
-     * interacts with the object.
-     */
-    rotation: 0,
-
-    /**
-     * The rotation value that will be set if the user releases the object at
-     * its current position.
-     */
-    currentDetent: null as number | null,
-
-    /**
-     * Resting means nothing is happening. Rotating means the user is
-     * interacting with the object. Transitioning means the user has released
-     * the object and the object is automatically moving to the nearest detent.
-     */
-    status: 'resting' as 'resting' | 'rotating' | 'transitioning',
-
-    /**
-     * When the object is a rest, its rotation is 0, so the rotation when the
-     * user grabs the object is almost always 0. It will be non-zero in cases
-     * where the user grabs the object during the object's final transition to
-     * the nearest detent after the user releases it.
-     */
-    rotationWhenGrabbed: null as number | null,
-
-    /**
-     * The angle of the user's mouse or touch at the point of the grab. Will be
-     * null when the user hasn't yet grabbed the object.
-     */
-    initialGrabAngle: null as number | null,
-
-    /**
-     * Stores the ID of the pointer used to grab the object.
-     */
-    pointerId: null as number | null,
-
-  }));
+  
+  /**
+   * The point (in page space) around which the object rotates. Null when we
+   * don't know yet.
+   */
+  const [center, setCenter] = useState<XyPoint | null>(null);
 
   /**
-   * We're using native events instead of React events because I couldn't
-   * figure out how to use setPointerCapture with React events. Because we're
-   * using native events, we need to add (and remove, within `tearDown`) the
-   * appropriate event handlers manually.
+   * The current value of rotation, which changes constantly as the user
+   * interacts with the object.
    */
-  const setUp = () => {
-    if (!ref.current) {return;}
-    const el = ref.current;
-
-    el.ongotpointercapture = () => console.log('got capture');
-    el.onlostpointercapture = () => console.log('lost capture');
-
-    el.onpointerdown = handlePointerDown;
-    el.onpointerup = handlePointerUp;
-    el.onpointercancel = handlePointerUp;
-  };
+  const [rotation, setRotation] = useState(0);
 
   /**
-   * Run setUp() when component mounts.
+   * The rotation value that will be set if the user releases the object at
+   * its current position.
    */
-  useEffect(setUp);
+  const [currentDetent, setCurrentDetent] = useState<number | null>(null);
 
-  const handlePointerDown = (e: PointerEvent) => {
+  /**
+   * Resting means nothing is happening. Rotating means the user is
+   * interacting with the object. Transitioning means the user has released
+   * the object and the object is automatically moving to the nearest detent.
+   */
+  type StatusOptions = 'resting' | 'rotating' | 'transitioning';
+  const [status, setStatus] = useState<StatusOptions>('resting');
+
+  /**
+   * When the object is a rest, its rotation is 0, so the rotation when the
+   * user grabs the object is almost always 0. It will be non-zero in cases
+   * where the user grabs the object during the object's final transition to
+   * the nearest detent after the user releases it.
+   */
+  const [rotationWhenGrabbed, setRotationWhenGrabbed] =
+    useState<number | null>(null);
+
+  /**
+   * The angle of the user's mouse or touch at the point of the grab. Will be
+   * null when the user hasn't yet grabbed the object.
+   */
+  const [initialGrabAngle, setInitialGrabAngle] =
+    useState<number | null>(null);
+
+
+  // const state = useLocalStore(() => ({
+
+  //   /**
+  //    * The point (in page space) around which the object rotates. Null when we
+  //    * don't know yet.
+  //    */
+  //   center: null as XyPoint | null,
+
+  //   /**
+  //    * The current value of rotation, which changes constantly as the user
+  //    * interacts with the object.
+  //    */
+  //   rotation: 0,
+
+  //   /**
+  //    * The rotation value that will be set if the user releases the object at
+  //    * its current position.
+  //    */
+  //   currentDetent: null as number | null,
+
+  //   /**
+  //    * Resting means nothing is happening. Rotating means the user is
+  //    * interacting with the object. Transitioning means the user has released
+  //    * the object and the object is automatically moving to the nearest detent.
+  //    */
+  //   status: 'resting' as 'resting' | 'rotating' | 'transitioning',
+
+  //   /**
+  //    * When the object is a rest, its rotation is 0, so the rotation when the
+  //    * user grabs the object is almost always 0. It will be non-zero in cases
+  //    * where the user grabs the object during the object's final transition to
+  //    * the nearest detent after the user releases it.
+  //    */
+  //   rotationWhenGrabbed: null as number | null,
+
+  //   /**
+  //    * The angle of the user's mouse or touch at the point of the grab. Will be
+  //    * null when the user hasn't yet grabbed the object.
+  //    */
+  //   initialGrabAngle: null as number | null,
+
+  // }));
+
+  // /**
+  //  * We're using native events instead of React events because I couldn't
+  //  * figure out how to use setPointerCapture with React events. Because we're
+  //  * using native events, we need to add (and remove, within `tearDown`) the
+  //  * appropriate event handlers manually.
+  //  */
+  // const setUp = () => {
+  //   if (!ref.current) {return;}
+  //   const el = ref.current;
+
+  //   el.ongotpointercapture = () => console.log('got capture');
+  //   el.onlostpointercapture = () => console.log('lost capture');
+
+  //   el.onpointerdown = handlePointerDown;
+  //   el.onpointerup = handlePointerUp;
+  //   el.onpointercancel = handlePointerUp;
+  // };
+
+  // /**
+  //  * Run setUp() when component mounts.
+  //  */
+  // useEffect(setUp);
+
+  const startRotating = (e: React.PointerEvent) => {
     e.preventDefault();
-
-    const okToProceed = 
-      e.button === 0 && // Don't rotate in response to right or middle clicks.
-      (state.status === 'resting' || state.status === 'transitioning') &&
-      !!ref.current; // We need to have a valid ref to the DOM element.
-    if (!okToProceed) {
-      transitionToRest();
+    e.persist()
+    const el = e.target as SVGGElement;
+    el.setPointerCapture(e.pointerId);
+    
+    if (e.button !== 0 || status === 'rotating') {
+      // Don't rotate in response to right-clicks, and also give up if we've got
+      // more than one touch.
+      transitionToRest(e);
       return;
     }
-    const el = ref.current as SVGGElement; // We know this isn't null from above
-    state.pointerId = e.pointerId;
-    state.center = centerOfContainingSvg(e);
-    state.initialGrabAngle = pointerGrabAngle(e);
-    state.status = 'rotating';
-    state.rotationWhenGrabbed = state.rotation;
-    console.log(el);
-    console.log(e.pointerId);
-    console.log(el.setPointerCapture)
-    // el.onpointermove = handlePointerMove;
-    el.setPointerCapture(e.pointerId);
+    // setCenter(centerOfContainingSvg(e));
+    // setInitialGrabAngle(pointerGrabAngle(e));
+    setStatus('rotating');
+    // setRotationWhenGrabbed(rotation);
+    console.log('down');
+    // console.log(el);
+    // console.log(e.pointerId);
+    // console.log(el.setPointerCapture)
   };
 
   /**
    * This is the function that gets called over and over while the user
    * interacts with the object.
    */
-  const handlePointerMove = (e: PointerEvent) => {
+  const updateRotationFromPointer = (e: React.PointerEvent) => {
+    
     console.log('move');
-    if (state.status !== 'rotating') { return; }
-
-    // if (!ref.current?.hasPointerCapture(e.pointerId)) {return;}
-
-    e.preventDefault();
-    const grabAngle = pointerGrabAngle(e);
-    if (!grabAngle) { transitionToRest(); return; }
+    // if (status !== 'rotating') { return; }
     
-    state.rotation = grabAngle +
-      (state.rotationWhenGrabbed || 0) - (state.initialGrabAngle || 0);
+    // e.preventDefault();
+    // const grabAngle = pointerGrabAngle(e);
+    // if (!grabAngle) { transitionToRest(e); return; }
     
-    state.currentDetent = Scalar.wrapToOctave(
-      props.detents ?
-        Ordinal.nearestValid(state.rotation, props.detents) :
-        // If we don't have detents then assume integers are detents.
-        Math.round(state.rotation)
-    );
+    // rotation = grabAngle +
+    //   (rotationWhenGrabbed || 0) - (initialGrabAngle || 0);
+    
+    // currentDetent = Scalar.wrapToOctave(
+    //   props.detents ?
+    //     Ordinal.nearestValid(rotation, props.detents) :
+    //     // If we don't have detents then assume integers are detents.
+    //     Math.round(rotation)
+    // );
   };
 
   /**
-   * Called when the user releases the object.
-   */
-  const handlePointerUp = (e: PointerEvent) => {
-    // TODO what if a touch different touch is released over this object while
-    // this object is rotating? Can we ignore it by validating e.pointerId?
-
-    
-    transitionToRest();
-  }
-
-  /**
    * Given a pointer event, and assuming we have already set the position of the
-   * rotation center as `state.center`, then compute the grab angle of the
+   * rotation center as `center`, then compute the grab angle of the
    * pointer's position.
    */
-  const pointerGrabAngle = (e: PointerEvent) => {
-    if (!state.center) {return null;}
-    return (new XyPoint(e.clientX, e.clientY)).minus(state.center).toI();
+  const pointerGrabAngle = (e: React.PointerEvent) => {
+    if (!center) {return null;}
+    return (new XyPoint(e.clientX, e.clientY)).minus(center).toI();
   }
 
   /**
    * Stop user interaction and transition gradually to the nearest detent.
    */
-  const transitionToRest = () => {
-    if (ref.current) {
-      ref.current.onpointermove = null;
-      if (state.pointerId) {
-        ref.current.releasePointerCapture(state.pointerId);
-      }
+  const transitionToRest = (e: React.PointerEvent) => {
+    console.log('up');
+    const el = e.target as SVGGElement;
+    if (el.hasPointerCapture(e.pointerId)) {
+      el.releasePointerCapture(e.pointerId);
     }
-    if (state.status !== 'rotating') {
+    if (status !== 'rotating') {
       // If we're at rest or (somehow) already transitioning, then no need to do
       // anything else.
       return;
     }
-    state.initialGrabAngle = null;
-    const rotation = state.rotation;
-    state.status = 'resting';
-    state.rotation = 0; // TODO transition to 0 gradually
-    const detent = state.currentDetent ?? rotation;
-    state.currentDetent = null;
+    setInitialGrabAngle(null);
+    setStatus('resting');
+    setRotation(0); // TODO transition to 0 gradually
+    const detent = currentDetent ?? rotation;
+    setCurrentDetent(null);
     props.afterRotating(detent);
   };
 
@@ -248,24 +270,30 @@ export function useRotator(props: Props) {
      * Gives the rotation value at which the object will come to rest after
      * the user releases the object at its current position.
      */
-    currentDetent: state.currentDetent,
+    currentDetent: currentDetent,
 
     /**
      * True when the user is interacting with the object, and also true when the
      * object is transitioning to rest after the user has released it.
      */
-    isRotating: state.status !== 'resting',
+    isRotating: status !== 'resting',
 
     /**
      * A React component used to wrap the SVG element that we want to rotate.
      */
     Container(p: {children: (props: ContainerProps) => JSX.Element}) {
-      return useObserver(() =>
+      return (
         <Group
-          rotation={state.rotation}
+          rotation={rotation}
+          onPointerDown={startRotating}
+          onPointerUp={transitionToRest}
+          onPointerCancel={transitionToRest}
+          onPointerMove={updateRotationFromPointer}
+          onGotPointerCapture={() => console.log('captured')}
+          onLostPointerCapture={() => console.log('released')}
           ref={ref}
         >
-          {p.children({rotation: state.rotation})}
+          {p.children({rotation: rotation})}
         </Group>
       );
     }, // Container
