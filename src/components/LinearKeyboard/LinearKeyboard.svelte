2f4823be4db45bb34c329ae93763a78374ace84f
Sean Colsen <colsen.sean@gmail.com>
2020-09-21 19:40:50 -0400

    Display multiple octaves on the linear keyboard

--- src/components/LinearKeyboard/LinearKeyboard.svelte
+++ src/components/LinearKeyboard/LinearKeyboard.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   import {noteSet, tonalCenter} from '../../store';
   import Key from '../Keyboard/Key.svelte';
-  $: pitches = $noteSet.notes.map(note => 
-    note.pitchAboveTonalCenterInOctave($tonalCenter, 4)
-  );
+
+  const pitchesInOctave = (octave: number) => $noteSet.notes.map(note => 
+    note.pitchAboveTonalCenterInOctave($tonalCenter, octave)
+  )
+  
+  $: pitches = [
+    ...pitchesInOctave(4),
+    ...pitchesInOctave(5),
+    $noteSet.notes[0].pitchAboveTonalCenterInOctave($tonalCenter, 6)
+  ];
 </script>
 
 <div class='keyboard'>
@@ -12,23 +19,32 @@
       <div
         class='key touch-receptor'
         class:isBlack={pitch.note.color === 'black'}
+        class:isTonalCenter={pitch.note.id === $tonalCenter}
       >
         {pitch.note.name?.unicode}
+        <sub>{pitch.spiOctave}</sub>
       </div>
     </Key>
   {/each}
 </div>
 
 <style>
-  .keyboard {display: flex; flex-direction: row;}
+  .keyboard {
+    display: flex;
+    flex-direction: row;
+    overflow-x: scroll;
+    height: 12vmax;
+  }
   .key {
     background: white;
     border: solid 0.3vmax #999;
-    width: 6vmax; height: 6vmax;
+    width: 6vmax;
+    height: 60%;
     cursor: pointer;
     text-align: center;
-    font-size: 3vmax;
+    font-size: 2.4vmax;
     border-radius: 0 0 1vmax 1vmax;
   }
+  .key.isTonalCenter {height: 90%}
   .key.isBlack {background: black; color: white;}
 </style>
