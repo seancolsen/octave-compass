6517bddce3a79365341ee460c63792b65e6d11a5
Sean Colsen <colsen.sean@gmail.com>
2020-09-23 21:02:39 -0400

    Fix radial spacing of chord emblems

--- src/components/Wheel/Scale/SliceOfChords.svelte
+++ src/components/Wheel/Scale/SliceOfChords.svelte
@@ -4,33 +4,53 @@
   import ChordInScale from './ChordInScale.svelte';
 
   const constellationRadius = 270;
-  const maxRadialSpacing = 5;
   const circleRadius = 45;
-  const centerDeadZone = 10;
 
   export let chordSet: ChordSet;
   export let ordinal: number;
   export let note: Note | undefined = undefined;
 
-  $: radialSpacing = (() => {
-    const total = chordSet.totalEmblemSize * circleRadius * 2;
-    const overShoot = constellationRadius - centerDeadZone - total;
-    const spacing = overShoot / (chordSet.count - 1);
-    return Math.min(spacing, maxRadialSpacing);
+  /**
+   * If 0, the emblems will be stacked edge to edge. When positive, the emblems
+   * will overlap by that amount. We need them to overlap more when more emblems
+   * are present, so we re-calculate this value as needed.
+   */
+  $: overlap = (() => {
+    const centerDeadZone = 30;
+    const availableSpace = constellationRadius - centerDeadZone;
+    const spaceNeededIfStacked = chordSet.totalEmblemSize * circleRadius * 2;
+    const totalOverlap = spaceNeededIfStacked - availableSpace;
+    const requiredOverlap = totalOverlap / (chordSet.count - 1);
+    const minOverlap = 10; // for aesthetics with few chords
+    return Math.max(minOverlap, requiredOverlap);
   })();
+
+  /**
+   * A radial position for each chord, matched by index.
+   */
   $: radialPositions = (() => {
-    let prevPosition = constellationRadius + radialSpacing;
+    // Place outer chords first.
+    // Each radial position corresponds to the chord emblem's center.
+
+    // Begin with an outer circle upon which the outer edge of the outer-most
+    // chord will be aligned.
+    let outerBound = constellationRadius;
+
     return chordSet.chords.map(chord => {
-      const size = chord.emblemSize * circleRadius;
-      const position = prevPosition - size + radialSpacing;
-      prevPosition = position;
-      return position;
+      const emblemRadius = chord.emblemSize * circleRadius;
+
+      // Move from the outer edge of the emblem to the center of the emblem.
+      const radialPosition = outerBound - emblemRadius;
+
+      // Move the outer bound inwards in preparation for the next emblem.
+      outerBound -= emblemRadius * 2 - overlap;
+      return radialPosition;
     });
   })();
 </script>
 
 <g>
-  {#each chordSet.chords as chord, index (index)}
+  {#each chordSet.chords as chord, index}
   <ChordInScale
     {note}
     radialPosition={radialPositions[index]}
